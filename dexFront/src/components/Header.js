98cb4b20c8ff9b424c5b4a0136dda67e6d11d5f9
Mustafa Zahid <mustafazahid167@gmail.com>
2024-06-30 14:39:33 +0500

    Header created

--- dexFront/src/components/Header.js
+++ dexFront/src/components/Header.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import Logo from "../moralis-logo.svg";
+import Eth from "../eth.svg";
+
 
 function Header() {
-  return (
-    <div>Header</div>
+  return(
+ <header>
+  <div className='leftH'>
+    <img src={Logo} alt="logo" className="logo" />
+    <div className="headerItem">Swap</div>
+    <div className="headerItem">Tokens</div>
+  </div>
+  <div className='rightH'>
+    <div className="headerItem">
+            <img src={Eth} alt="eth" className="eth" />
+            Ethereum
+    </div>
+          <div className="connectButton">
+            Connect
+          </div>
+  </div>
+ </header>
   )
 }
 
