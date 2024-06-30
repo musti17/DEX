36442c9fddcb2ea217d2d1561e405f968a5d8e52
Mustafa Zahid <mustafazahid167@gmail.com>
2024-07-01 01:33:13 +0500

    Implemented token price fetching with USD price and ratio calculation. Backend for fetching token prices complete..Now have to glue it with the frontend

--- dexBack/index.js
+++ dexBack/index.js
@@ -21,10 +21,16 @@ app.get("/tokenPrice", async (req, res) => {
       address: query.addressTwo
     });
 
+    const usdPrices = {
+      tokenOne:responseOne.raw.usdPrice,
+      tokenTwo:responseTwo.raw.usdPrice,
+      ratio:responseOne.raw.usdPrice/responseTwo.raw.usdPrice
+    }
+
     console.log(responseOne.raw);
     console.log(responseTwo.raw);
 
-    return res.status(200).json({});
+    return res.status(200).json({usdPrices});
   } catch (error) {
     console.error("Error fetching token prices:", error);
     return res.status(500).json({ error: "Error fetching token prices" });
