8cc15a1a5efda6f2529e0718f5e9fa1634f2a920
Mustafa Zahid <mustafazahid167@gmail.com>
2024-06-30 14:53:53 +0500

    Add navigation with react-router-dom to Swap and Tokens pages
    - Implement <Routes> in app.js to handle "/" and "/tokens" paths
    - Create navigation links in header.js for Swap and Tokens
    - Ensure smooth navigation between pages

--- dexFront/src/App.js
+++ dexFront/src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
 import Header from "./components/Header";
+import Swap from "./components/Swap";
+import Tokens from "./components/Tokens";
+import {Routes,Route} from "react-router-dom";
 
 function App() {
   return(
-    <Header/>
+    <div className="App">
+      <Header/>
+      <div className="mainWindow">
+        <Routes>
+          <Route path="/" element={<Swap/>}/>
+          <Route path="/tokens" element={<Tokens/>}/>
+        </Routes>
+      </div>
+    </div>
   )
 }
 
--- dexFront/src/components/Header.js
+++ dexFront/src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Logo from "../moralis-logo.svg";
 import Eth from "../eth.svg";
+import {Link} from "react-router-dom";
 
 
 function Header() {
@@ -8,8 +9,12 @@ function Header() {
  <header>
   <div className='leftH'>
     <img src={Logo} alt="logo" className="logo" />
+    <Link to="/" className='link'>
     <div className="headerItem">Swap</div>
+    </Link>
+    <Link to="/tokens" className='link'>
     <div className="headerItem">Tokens</div>
+    </Link>
   </div>
   <div className='rightH'>
     <div className="headerItem">
