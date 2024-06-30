058e447fb6de8190fbd3202c1f23c4d6ba248953
Mustafa Zahid <mustafazahid167@gmail.com>
2024-07-01 01:26:17 +0500

    Configured Moralis API key in .env file and implemented fetching token prices

--- dexBack/index.js
+++ dexBack/index.js
@@ -8,14 +8,33 @@ const port = 3001;
 app.use(cors());
 app.use(express.json());
 
+
 app.get("/tokenPrice", async (req, res) => {
-  return res.status(200).json(usdPrices);
+  const { query } = req;
+
+  try {
+    const responseOne = await Moralis.EvmApi.token.getTokenPrice({
+     address: query.addressOne
+    });
+
+    const responseTwo = await Moralis.EvmApi.token.getTokenPrice({
+      address: query.addressTwo
+    });
+
+    console.log(responseOne.raw);
+    console.log(responseTwo.raw);
+
+    return res.status(200).json({});
+  } catch (error) {
+    console.error("Error fetching token prices:", error);
+    return res.status(500).json({ error: "Error fetching token prices" });
+  }
 });
 
 Moralis.start({
   apiKey: process.env.MORALIS_KEY,
 }).then(() => {
   app.listen(port, () => {
-    console.log(`Listening for API Calls`);
+    console.log(`Listening for API Calls on port ${port}`);
   });
 });
