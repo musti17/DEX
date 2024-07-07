66ba1658937644a4f5cdf2459af196e73ef9abf8
Mustafa Zahid <mustafazahid167@gmail.com>
2024-07-07 15:07:37 +0500

    Configured blockchain to Sepolia test network

--- dexFront/src/index.js
+++ dexFront/src/index.js
@@ -3,11 +3,32 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { configureChains, mainnet, WagmiConfig, createClient } from "wagmi";
+import { configureChains, WagmiConfig, createClient } from "wagmi";
 import { publicProvider } from "wagmi/providers/public";
 
+// Define the Sepolia testnet configuration
+const sepolia = {
+  id: 11155111,
+  name: 'sepolia',
+  network: 'sepolia',
+  nativeCurrency: {
+    name: 'Sepolia Ether',
+    symbol: 'ETH',
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://rpc.sepolia.org'],
+    },
+  },
+  blockExplorers: {
+    default: { name: 'Etherscan', url: 'https://sepolia.etherscan.io' },
+  },
+  testnet: true,
+};
+
 const { provider, webSocketProvider } = configureChains(
-  [mainnet],
+  [sepolia],
   [publicProvider()]
 );
 
@@ -20,7 +41,7 @@ const client = createClient({
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-       <WagmiConfig client={client}>
+    <WagmiConfig client={client}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
